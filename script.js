fa0d22c2cad17671cdc0dd3f6cb9a191a8e22996
cravsky <cravsky@gmail.com>
2024-07-01 20:09:46 +0200

    Getting data from Unsplash API

---
+++ script.js
@@ -0,0 +1,21 @@
+// Unsplash API
+const count = 5;
+const apiKey = 'nVCPp2bosPtaXsv8fhMSsGqiMbuBfPfSESvMRch-O28'
+
+const apiUrl = `https://api.unsplash.com/photos/random?
+client_id=${apiKey}&count=${count}`;
+
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+
+// On Load
+getPhotos();
